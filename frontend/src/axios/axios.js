bb0d669a8998550f0c68c4b4ecddaf7d0759fde8
nadiradad <nadiradad@gmail.com>
2025-06-08 22:50:28 -0300

    ruta correcta

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost/NetflixStatistics/backend/api/",
+  baseURL: "http://localhost/netflix/NetflixStatistics/backend/api/",
   headers: {
     "Content-Type": "application/json",
   },
