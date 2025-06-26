b71ddbe54bd0d6c971f4620eeb82945389bd8f2e
moonstone3798 <moonstone3798@gmail.com>
2025-06-26 17:30:01 -0300

    Error in npm

--- frontend/tests/unit/useUser.test.js
+++ frontend/tests/unit/useUser.test.js
@@ -20,9 +20,9 @@ describe("useUser", () => {
 
   it("valida formato de contraseña correctamente", () => {
     expect(validatePasswordFormat("abc123")).toBe(true);
-    expect(validatePasswordFormat("123456")).toBe(false); // solo números
-    expect(validatePasswordFormat("abcdef")).toBe(false); // solo letras
-    expect(validatePasswordFormat("a1")).toBe(false); // muy corta
+    expect(validatePasswordFormat("123456")).toBe(false);
+    expect(validatePasswordFormat("abcdef")).toBe(false);
+    expect(validatePasswordFormat("a1")).toBe(false);
   });
 
   it("devuelve mensaje correcto si email es vacío", () => {
@@ -38,39 +38,41 @@ describe("useUser", () => {
   });
 
   it("devuelve mensaje si contraseña es muy larga", () => {
-    const error = passwordValidations.find((v) => v.condition("a1".repeat(11))); // 22 chars
+    const error = passwordValidations.find((v) => v.condition("a1".repeat(11)));
     expect(error.message).toBe(
       "La contraseña no puede tener más de 20 caracteres"
     );
   });
+
   it("devuelve mensaje si las contraseñas no coinciden", () => {
     const error = repeatPasswordValidations.find((v) =>
       v.condition("acd123", "123acd")
     );
     expect(error.message).toBe("Las contraseñas deben coincidir");
   });
+
   it("devuelve mensaje si el nombre tiene menos de 3 caracteres", () => {
     const error = nameValidations.find((v) => v.condition("Lu"));
     expect(error.message).toBe("El nombre debe tener al menos 3 caracteres");
   });
+
   it("devuelve mensaje si el apellido tiene menos de 2 caracteres", () => {
     const error = lastNameValidations.find((v) => v.condition("L"));
     expect(error.message).toBe("el apellido debe tener al menos 2 caracteres");
   });
+
   it("devuelve mensaje si se realiza registro sin contraseña", () => {
     const error = passwordValidations.find((v) => v.condition(""));
     expect(error.message).toBe("La contraseña es obligatoria");
   });
+
   it("devuelve mensaje si se realiza registro con un nombre de más de 20 caracteres", () => {
-    const error = nameValidations.find((v) =>
-      v.condition(
-        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-      )
-    );
+    const error = nameValidations.find((v) => v.condition("A".repeat(100)));
     expect(error.message).toBe(
       "El nombre debe tener como máximo 20 caracteres"
     );
   });
+
   it("devuelve mensaje correcto si email no tiene su respectivo formato", () => {
     const error = emailValidations.find((v) => v.condition("asd"));
     expect(error.message).toBe("El email no tiene un formato válido");
