2f3d25a7c257b3fc8ee266f16089fafbce781525
nadiradad <nadiradad@gmail.com>
2025-06-29 18:19:20 -0300

    post tipos prod

--- backend/api/tipos_producciones/post_tipos_produccion.php
+++ backend/api/tipos_producciones/post_tipos_produccion.php
@@ -1,14 +1,18 @@
 <?php
 require __DIR__ . '/../../config/conexion.php';
 header('Content-Type: application/json');
-// Configurar CORS correctamente
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Max-Age: 86400"); // 1 día
+header("Access-Control-Max-Age: 86400");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 try {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
         $input = file_get_contents("php://input");
         $data = json_decode($input, true);
 
